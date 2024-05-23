10fe531e2c363be8f4905eabc5688b81c5e22484
JcTheSavior <jacobconnell3@gmail.com>
2024-05-23 16:37:53 -0400

    Add; schema for users signup/login/logout
    Schema to set limits on what type of email and username users can have

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,30 @@
 //Handling the Schema being used for users involved in CRUD operations
+//--------------------------{imports}-------------
+import mongoose from "mongoose";
+
+//-------Define Schema
+const userSchema = new mongoose.Schema({
+    emails: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+
