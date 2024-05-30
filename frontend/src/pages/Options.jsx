19c5a8340c554b25baec35c6f7d658bce3b79864
JcTheSavior <jacobconnell3@gmail.com>
2024-05-29 23:31:34 -0400

    Add; Options page links and styling for options

--- frontend/src/pages/Options.jsx
+++ frontend/src/pages/Options.jsx
@@ -1,6 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Options = () => {
   return (
-    <div>Options</div>
+    <div className="optionsPageContainer">
+      <h2 className="optionsTitle">Pick your Poison</h2>
+      <div className="optionsDescr">Below you will find a list of current options implemented in the site.</div>
+      <ul className="optionsList">
+        <li className="optionListItem1">
+          <Link to='/login'>
+            <span className="optionLink1">Login</span>
+          </Link>
+        </li>
+        <li className="optionListItem2">
+          <Link to='/Create'>
+            <span className="optionLink2">Create Content</span>
+          </Link>
+        </li>
+        <li className="optionListItem3">
+          <Link to='/usersContent'>
+            <span className="optionLink3">Content made by users like you!</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 export default Options
