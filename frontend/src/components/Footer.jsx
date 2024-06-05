a56c7f8492b3ba84993bc359097f9705eec5c028
JcTheSavior <jacobconnell3@gmail.com>
2024-06-05 19:43:01 -0400

    FIX; background images moved to public/assets dir
    This allows vite to use the background-images during build for
    production.  Was able to test this as well locally to prevent future
    excessive commits

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,23 +1,8 @@
-import footerImage from '../assets/footerImage.webp';
-import create5EBackground from '../assets/create5EBackground.webp';
-import createFormBackground from '../assets/createFormBackground.webp';
-import greenPoison from '../assets/greenPoison.gif';
-import loggedInBackground from '../assets/loggedInBackground.webp';
-import loginBackground from '../assets/loginBackground.webp';
-import signUpBackground from '../assets/signUpBackground.webp';
-
+import footerImage from '../assets/footerImage.webp'
 const Footer = () => {
   return (
     <div className="footerContainer">
         <img className="footerImg" src={footerImage}/>
-        <div className="footerHiddenContents">
-          <img src={create5EBackground} />
-          <img src={createFormBackground} />
-          <img src={greenPoison} />
-          <img src={loggedInBackground} />
-          <img src={loginBackground} />
-          <img src={signUpBackground} />
-        </div>
     </div>
   );
 };
