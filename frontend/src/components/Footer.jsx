2d595735ae60f4f4d20e7544edd9eceeaa29651b
JcTheSavior <jacobconnell3@gmail.com>
2024-06-05 18:55:21 -0400

    change; move files for correct usage with render

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,23 @@
-import footerImage from '../assets/footerImage.webp'
+import footerImage from '../assets/footerImage.webp';
+import create5EBackground from '../assets/create5EBackground.webp';
+import createFormBackground from '../assets/createFormBackground.webp';
+import greenPoison from '../assets/greenPoison.gif';
+import loggedInBackground from '../assets/loggedInBackground.webp';
+import loginBackground from '../assets/loginBackground.webp';
+import signUpBackground from '../assets/signUpBackground.webp';
+
 const Footer = () => {
   return (
     <div className="footerContainer">
         <img className="footerImg" src={footerImage}/>
+        <div className="footerHiddenContents">
+          <img src={create5EBackground} />
+          <img src={createFormBackground} />
+          <img src={greenPoison} />
+          <img src={loggedInBackground} />
+          <img src={loginBackground} />
+          <img src={signUpBackground} />
+        </div>
     </div>
   );
 };
